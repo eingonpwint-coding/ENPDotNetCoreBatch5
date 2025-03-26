766ee238ac13db5c45801aeea9cd8da2e2d62885
EiNgonPwint <eingonpwint13254@gmail.com>
2025-03-26 14:15:34 +0800

    feat : Dapper (Edit)

--- ENPDotNetCoreBatch5.ConsoleApp/DapperExample.cs
+++ ENPDotNetCoreBatch5.ConsoleApp/DapperExample.cs
@@ -68,6 +68,23 @@ namespace ENPDotNetCoreBatch5.ConsoleApp
             }
         }
 
+        public void Edit(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "select * from tbl_Blog where DeleteFlag=0 and BlogId = @BlogId;";
+                var item = db.Query<BlogDataModel>(query, new BlogDataModel
+                {
+                    BlogId = id
+                }).FirstOrDefault();
+
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+
+            }
+        }
 
     }
 }
