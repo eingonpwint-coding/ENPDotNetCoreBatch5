26eed21a5397c28700f1e178c59316968c8230c7
EiNgonPwint <eingonpwint13254@gmail.com>
2025-08-15 16:46:27 +0800

    feat : ASP.NET Core MVC using ApexChart

---
+++ ENPDotNetCoreBatch5.ChartWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ENPDotNetCoreBatch5.ChartWebApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
